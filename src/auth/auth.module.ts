117086f5a9dfd7d15b21ef7b1d3f7eaf0d02f731
eynullabeyli <eynullabeyliy@gmail.com>
2022-09-15 22:39:24 +0400

    added mongo to package

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserInformationController } from './../user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [UserInformationController],
   providers: [],
 })
